adeb2469d8829ff14a321f993703c73361b9bf54
JuanDavid74 <66858283+JuanDavid74@users.noreply.github.com>
2021-12-09 21:40:43 -0500

    Commit Final

--- Tarea1.c
+++ Tarea1.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
+
+int main(int argc, char *argv[]){
+
+pid_t pid;
+
+int contador = 1;
+pid = fork();
+
+  if(pid == 0){
+    contador += 5;
+  }else{
+    contador += 2;  
+  }
+ printf("[%d] El valor de contador es %i\n",getpid(),contador);
+  
+  return 0;
+}
